ddde65e3c28de782277a0c9350201418e10b49c1
Eduardo Magnus <eduj.magnus@gmail.com>
2024-03-06 13:32:10 -0300

    Changing firebase API key

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBy3qZjyyOsplQxQaFd1tHyCl4qc8R21X0",
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
     authDomain: "investy-dace1.firebaseapp.com",
     projectId: "investy-dace1",
     storageBucket: "investy-dace1.appspot.com",
